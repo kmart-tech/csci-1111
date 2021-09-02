6571ca75315eb7395c28f9143971cf4f6c156ac9
kmart-tech <89538364+kmart-tech@users.noreply.github.com>
2021-09-02 13:14:41 -0600

    Reorder and Rename Chapter 10 Files

---
+++ chapter10/Exercise10_07/Account.java
@@ -0,0 +1,49 @@
+class Account {
+	private int id = 0;
+	private double balance = 0.0;
+	private static double annualIntrestRate = 0.0; // saved as a percent
+	private java.util.Date dateCreated;
+	
+	Account(){
+		dateCreated = new java.util.Date();
+	}
+	
+	Account(int newId, int startBalance) {
+		id = newId;
+		balance = startBalance;
+		dateCreated = new java.util.Date();
+	}
+	
+	int getId() {return id;}
+	double getBalance() {return balance;}
+	double getAnnualInterestRate() {return annualIntrestRate;}
+	java.util.Date getDateCreated() {return dateCreated;}
+	
+	void setId(int newId) {
+		id = newId;
+	}
+	
+	void setBalance(double newBalance) {
+		balance = newBalance;
+	}
+	
+	static void setAnnualInterestRate(double newRate) {
+		annualIntrestRate = newRate;
+	}
+	
+	double getMonthlyInterestRate() {
+		return annualIntrestRate / 12;
+	}
+	
+	double getMonthlyInterest() {
+		return balance * (this.getMonthlyInterestRate() / 100);
+	}
+	
+	void withdraw(double amount) {
+		balance -= Math.abs(amount);
+	}
+	
+	void deposit(double amount) {
+		balance += Math.abs(amount);
+	}
+}
