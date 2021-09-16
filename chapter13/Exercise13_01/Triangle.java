15df1e1453277954ed1779dc1f704c55fbddb6bd
404kevnotfound <89538364+kmart-tech@users.noreply.github.com>
2021-09-16 13:48:01 -0600

    Add TestTriangle and add constuctor in Triangle

--- chapter13/Exercise13_01/Triangle.java
+++ chapter13/Exercise13_01/Triangle.java
@@ -3,7 +3,7 @@ Kevin Martinsen
 CSCI 1111 - OOP 1
 09/13/2021
 
-Exercise 11-01: Triangle Object
+Exercise 13-01: Triangle object for abstract GeometricObject
 */
 
 class Triangle extends GeometricObject {
@@ -19,6 +19,13 @@ class Triangle extends GeometricObject {
 		side3 = s3;
 	}
 	
+	Triangle(String color, boolean filled, double s1, double s2, double s3) {
+		super(color, filled);
+		side1 = s1;
+		side2 = s2;
+		side3 = s3;
+	}
+	
 	public void setSide1(double length) {side1 = length;}
 	public void setSide2(double length) {side2 = length;}
 	public void setSide3(double length) {side3 = length;}
@@ -27,6 +34,7 @@ class Triangle extends GeometricObject {
 	public double getSide2() {return side2;}
 	public double getSide3() {return side3;}
 	
+	@Override
 	public double getArea(){
 		double s = (side1 + side2 + side3) / 2;
 		return Math.pow(s * (s - side1) * (s - side2) * (s - side3),0.5);
@@ -37,6 +45,7 @@ class Triangle extends GeometricObject {
 		return side1 + side2 + side3;
 	}
 	
+	//Maybe call the GeometricObject to string and add the sides info?
 	@Override
 	public String toString(){
 		return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
